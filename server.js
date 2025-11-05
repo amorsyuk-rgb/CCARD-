c9034d8b28fd1a7dbc698d9292954234f6cadbfb
amorsyuk-rgb <amorsyuk@gmail.com>
2025-11-05 20:41:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 10000;
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "gracewise.html"));
+  res.sendFile(path.join(__dirname, "gracewise_v4.html"));
 });
 
-app.listen(PORT, () => console.log(`✅ GraceWise running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ GraceWise v4 running on port ${PORT}`));
