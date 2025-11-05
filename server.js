f2a1ce301f7b36a3d79e24ce3e9b2324d07684da
amorsyuk-rgb <amorsyuk@gmail.com>
2025-11-05 22:09:19 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'gracewise_v5_full.html')));
-app.listen(process.env.PORT || 10000, ()=>console.log('GraceWise v5 full fixed running'));
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname,'gracewise_v5_full.html')));
+app.listen(process.env.PORT||10000, ()=>console.log('GraceWise Safe Mode running'));
